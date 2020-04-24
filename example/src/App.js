d8633d151755fd0a6613486a21ce99bbc481e48d
Priyanka <Priyanka.bhavsar45@hotmail.com>
2020-04-24 21:44:56 +0530

    I have applyied simplae package comfigaration

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { CustomSweetAlert } from 'custom-sweet-alert'
+import 'custom-sweet-alert/dist/index.css'
+
+const App = () => {
+  return <CustomSweetAlert text="Create React Library Example 😄" />
+}
+
+export default App
